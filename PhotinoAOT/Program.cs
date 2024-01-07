bc6c6d558e7078f545cd86423e902a428e6edeca
Raghuveer Ch S <raghuveer@codeindians.com>
2024-01-07 16:45:14 +0530

    Show  single Photino window from a console app

---
+++ PhotinoAOT/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using PhotinoNET;
+
+Thread thread = new Thread(() => {
+    new PhotinoWindow()
+               .SetTitle("Main Window")
+               .RegisterWebMessageReceivedHandler(null)
+               .SetUseOsDefaultSize(false)
+               .SetWidth(600)
+               .SetHeight(400)
+               .Center()
+               .Load(new Uri("http://google.com"))
+               .WaitForClose();
+});
+thread.SetApartmentState(ApartmentState.STA);
+thread.Start();
+
+
+Console.ReadKey();
+thread.Join();
